c9e94cf930b300a9239e19ee7664b523eb9ef1de
saitoio <uwakr1129@gmail.com>
2022-04-10 15:21:56 +0900

    環境構築

---
+++ src/basic/primitive.ts
@@ -0,0 +1,10 @@
+export default function primitiveSample(){
+  let name: string = 'saito'
+  console.log("primitive sample 1:", typeof name, name)
+
+  let age: number = 27
+  console.log("primitive sample 2:", typeof age, age)
+
+  let isSingle: boolean = true
+  console.log("primitive sample 3:", typeof isSingle, isSingle)
+}
---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// import World from "./world";
+
+// const root = document.getElementById("root")
+// const world = new World("Hello World");
+// world.sayHello(root);
+
+//03.基本の型定義
+import { primitiveSample } from "./basic";
+
+primitiveSample()
